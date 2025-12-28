4ddbbaf9b83cd7f036fc90b64ca8ee9d7c9df58b
Adrien Tremblay <adrien@adrientremblay.com>
2025-12-28 14:33:21 -0500

    feat: WIP on undoing

--- scripts/undoRedoManager.js
+++ scripts/undoRedoManager.js
@@ -9,23 +9,37 @@ const UndoRedoManager = {
 
     recordState: (e) => {
         const node = e.target;
-        if (!node)
+        const nodeParent = e.target.parent.canvas;
+        console.log(e);
+
+        if (!node || !nodeParent)
             return;
 
-
-        UndoRedoManager.history.push(node.toJSON());
-        currentHistoryIndex++;
+        UndoRedoManager.history.push({
+            node:  node.toJSON(),
+            parent: parent,
+        });
+        UndoRedoManager.currentHistoryIndex++;
 
         console.log(UndoRedoManager.history);
     },
 
+    undo: () => {
+        if (UndoRedoManager.currentHistoryIndex < 0)
+            return;
+        
+        const historyElement = UndoRedoManager.history[UndoRedoManager.currentHistoryIndex--];
+        //historyElement.parent.create(historyElement.node);
+        Konva.Node.create(historyElement.node);
+        console.log("created node");
+    },
+
     init: () => {
-        console.log('init');
         document.addEventListener('keydown', (event) => {
             // Check if Ctrl or Command (Mac) is pressed along with 'z'
             if ((event.ctrlKey || event.metaKey) && event.key === 'z') {
                 event.preventDefault();
-                console.log('Ctrl+Z');
+                UndoRedoManager.undo();
             }
         }); 
     }
